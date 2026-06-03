0d58630b50ab34ac454a91713eb3a4ce0c0a72be
liubei66 <46340917+liubei66@users.noreply.github.com>
2026-06-03 16:25:01 +0800

    Update delete-stun.js

--- api/delete-stun.js
+++ api/delete-stun.js
@@ -1,14 +1,16 @@
 import { Redis } from '@upstash/redis';
-
 const redis = Redis.fromEnv();
+const SECRET = process.env.API_SECRET;
 
 export default async function handler(req, res) {
-  if (req.method !== 'POST') return res.status(405).end();
-
+  if (req.method !== 'POST') return res.status(405).json({success:false});
+  const auth = req.headers.authorization || '';
+  if(auth && auth !== `Bearer ${SECRET}`){
+    return res.status(401).json({success:false,msg:'密钥错误'})
+  }
   const { key } = req.body;
   const data = await redis.get('stun_list') || {};
   delete data[key];
   await redis.set('stun_list', data);
-
   res.json({ success: true });
 }
