6a3641df7a29791e27da4ca938290956f3e99750
liubei66 <46340917+liubei66@users.noreply.github.com>
2026-06-03 22:19:43 +0800

    Update [key].js

--- api/j/[key].js
+++ api/j/[key].js
@@ -4,7 +4,8 @@ const redis = Redis.fromEnv();
 export default async function handler(req, res) {
   try {
     // ✅ 正确：从 /j/xxx 拿到 xxx
-    const key = req.query.key;
+    const key = req.query;
+    console.log("key: ", key);
 
     if (!key) {
       return res.status(400).send("缺少key");
