d4d2affa8988be860892f5f8b09168725a1b4295
liubei66 <46340917+liubei66@users.noreply.github.com>
2026-06-03 11:45:37 +0800

    Update get-stun.js

--- api/get-stun.js
+++ api/get-stun.js
@@ -1,6 +1,9 @@
-import { kv } from '@vercel/kv'
+import { Redis } from '@upstash/redis';
+
+const redis = Redis.fromEnv();
+
 export default async function handler(req, res) {
-  if (req.method !== 'GET') return res.status(405).json({ success: false })
-  const data = await kv.get('stun_map') || {}
-  return res.json({ success: true, data })
+  if (req.method !== 'GET') return res.status(405).json({ success: false });
+  const data = await redis.get('stun_map') || {};
+  return res.json({ success: true, data });
 }
