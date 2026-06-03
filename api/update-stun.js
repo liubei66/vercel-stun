761ecc10ca28b9f0baf38ae919e9a26248af777a
liubei66 <46340917+liubei66@users.noreply.github.com>
2026-06-03 10:37:15 +0800

    Update update-stun.js

--- api/update-stun.js
+++ api/update-stun.js
@@ -1,16 +1,10 @@
+import { kv } from '@vercel/kv'
 export default async function handler(req, res) {
-  if (req.method !== 'POST') return res.status(405).json({ success: false });
-
-  try {
-    const { key, value } = req.body;
-    if (!key || !value) return res.status(400).json({ success: false });
-
-    let map = await process.env.KV.get('stun_map') || {};
-    map[key] = value;
-    await process.env.KV.put('stun_map', map);
-
-    return res.json({ success: true });
-  } catch (e) {
-    return res.status(500).json({ success: false });
-  }
+  if (req.method !== 'POST') return res.status(405).json({success:false})
+  const {key,value}=req.body
+  if(!key||!value) return res.status(400).json({success:false})
+  let map = await kv.get('stun_map')||{}
+  map[key]=value
+  await kv.set('stun_map',map)
+  return res.json({success:true})
 }
