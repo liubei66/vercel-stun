c8e91a1e47eb3b8b8cf3db5a5e847151e7851459
liubei66 <46340917+liubei66@users.noreply.github.com>
2026-06-03 16:46:48 +0800

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,15 +1,21 @@
 import { Redis } from '@upstash/redis';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs'; 
 const redis = Redis.fromEnv();
 
 export default async function handler(req, res) {
-  if (req.method !== 'POST') return res.status(405).json({success:false});
-  const {username,password} = req.body;
+  if (req.method !== 'POST') {
+    return res.status(405).json({ success: false });
+  }
+
+  const { username, password } = req.body;
   const user = process.env.LOGIN_USER;
   const hash = process.env.LOGIN_PWD_HASH;
 
-  if(username === user && await bcrypt.compare(password, hash)){
-    return res.json({success:true})
+  const isPwdOk = bcrypt.compareSync(password, hash);
+
+  if (username === user && isPwdOk) {
+    return res.json({ success: true });
   }
-  return res.json({success:false})
+
+  return res.json({ success: false });
 }
