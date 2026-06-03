34ef696d3a93de6d4f416e0fcbe71e884e6be0de
liubei66 <46340917+liubei66@users.noreply.github.com>
2026-06-03 16:58:21 +0800

    Update get-stun.js

--- api/get-stun.js
+++ api/get-stun.js
@@ -1,13 +1,14 @@
 import { Redis } from '@upstash/redis';
 const redis = Redis.fromEnv();
-const SECRET = process.env.API_SECRET;
 
 export default async function handler(req, res) {
-  const auth = req.headers.authorization || '';
-  const needAuth = !!auth;
-  if(needAuth && auth !== `Bearer ${SECRET}`){
-    return res.status(401).json({success:false,msg:'密钥错误'})
+  const token = req.headers.authorization;
+  const correctToken = process.env.API_TOKEN;
+
+  if (token && token !== correctToken) {
+    return res.status(401).json({ success: false });
   }
+
   const data = await redis.get('stun_list') || {};
   res.json({ success: true, data });
 }
