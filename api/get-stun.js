8ca52b36a291bbbb2acc3b818923ae02c87f2c9c
liubei66 <46340917+liubei66@users.noreply.github.com>
2026-06-03 17:20:22 +0800

    Update get-stun.js

--- api/get-stun.js
+++ api/get-stun.js
@@ -3,10 +3,19 @@ const redis = Redis.fromEnv();
 
 export default async function handler(req, res) {
   const token = req.headers.authorization;
-  const correctToken = process.env.API_TOKEN;
+  const API_TOKEN = process.env.API_TOKEN;
 
-  if (token && token !== correctToken) {
-    return res.status(401).json({ success: false });
+  let allowed = false;
+
+  if (token === API_TOKEN) {
+    allowed = true;
+  } else {
+    const valid = await redis.get(`login:${token}`);
+    if (valid === 'ok') allowed = true;
+  }
+
+  if (!allowed) {
+    return res.status(401).json({ success: false, msg: '请登录' });
   }
 
   const data = await redis.get('stun_list') || {};
