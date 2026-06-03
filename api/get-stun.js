afaccd5ea6bd2f3d5a2fec497bb62f31d672f021
liubei66 <46340917+liubei66@users.noreply.github.com>
2026-06-03 10:49:39 +0800

    Update get-stun.js

--- api/get-stun.js
+++ api/get-stun.js
@@ -1,10 +1,11 @@
 import { kv } from '@vercel/kv'
+
 export default async function handler(req, res) {
-  if (req.method !== 'GET') return res.status(405).json({success:false})
+  if (req.method !== 'GET') return res.status(405).json({ success: false })
   try {
-    const stunMap = await kv.get('stun_map') || {}
-    return res.json({success:true,data:stunMap})
-  }catch(e){
-    return res.status(500).json({success:false})
+    const data = await kv.get('stun_map') || {}
+    return res.json({ success: true, data })
+  } catch (e) {
+    return res.status(500).json({ success: false })
   }
 }
