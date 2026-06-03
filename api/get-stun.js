5e4d2202f042bfbe2e46a912b129d790cbe0582c
liubei66 <46340917+liubei66@users.noreply.github.com>
2026-06-03 10:27:09 +0800

    Create get-stun.js

---
+++ api/get-stun.js
@@ -0,0 +1,11 @@
+export default async function handler(req, res) {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ success: false });
+  }
+  try {
+    const stunMap = await process.env.KV.get('stun_map') || {};
+    return res.json({ success: true, data: stunMap });
+  } catch (e) {
+    return res.status(500).json({ success: false });
+  }
+}
